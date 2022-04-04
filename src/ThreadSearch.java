8f2a59243a8b478b256dbafaeac8ad10a09d0268
Renan <renanfigcarneiro@gmail.com>
2022-04-03 23:10:16 -0300

    adiciona comentarios e logica de finalizacao para varias resultados

--- src/ThreadSearch.java
+++ src/ThreadSearch.java
@@ -7,10 +7,12 @@ import java.text.NumberFormat;
 
 public class ThreadSearch extends Thread {
 	
+	// Variáveis privadas da classe
 	private final String pasta;
 	private final String arquivo;
 	private final String threadName = Thread.currentThread().getName();
 
+	// Construtor da classe que também realiza o start da thread e o join na main
 	public ThreadSearch(String pasta,
 						String arquivo) {
 		this.arquivo = arquivo;
@@ -23,23 +25,30 @@ public class ThreadSearch extends Thread {
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
 	}
 	
+	// Função de busca
 	public void busca(String arquivo,
 					  String pasta) {
+		// Faz o parse do Path que o arquivo via ser buscado
 		Path currentRelativePath = Paths.get(pasta);
+		// Transforma o path em um arquivo para que possa ser manipulado
 		File dir = new File(currentRelativePath.toAbsolutePath().toString());
+		// Filtra os arquivos de acordo com parametros
 		File[] matches = dir.listFiles(new FilenameFilter() {
 			
+			// Parametros de aceitacao
 			@Override
 			public boolean accept(File dir, String name) {
 				return name.startsWith(arquivo);
 			}
 		});
+		
+		// Se nao for encontrado o arquivo desejado, o codigo ira entrar na condicao
 		if (matches == null || matches.length == 0) {
 			System.out.println("Não encontramos na thread: " + this.threadName);
 			try {
+				// Filtra somente os diretorios
 				File[] dirFound = dir.listFiles(new FilenameFilter() {
 					
 					@Override
@@ -47,6 +56,7 @@ public class ThreadSearch extends Thread {
 						return dir.isDirectory();
 					}
 				});
+				// Faz uma chamada recursiva onde a classe se instancia para cada diretorio dentro do path em que estamos
 				for(File oneDir : dirFound) {
 					new ThreadSearch(oneDir.getAbsolutePath(), arquivo);
 				}
@@ -57,15 +67,20 @@ public class ThreadSearch extends Thread {
 			
 		}
 		
+		int i = 0;
+		// Executa quando encontra alguma resposta que satisfaz as condicoes
         for(File f : matches) {
-            System.out.println("Achei:" + f.getName());
-            System.out.println("Na Thread:" + this.threadName);
-            System.out.println(f.getAbsolutePath());
+            System.out.println("Arquivo encontrado:" + f.getName());
+            System.out.println("Na Thread: " + this.threadName);
+            System.out.println("Na Pasta: " + f.getAbsolutePath());
             NumberFormat formatter = new DecimalFormat("#0.00000");
             System.out.println("Encontrado em: " + 
             formatter.format(((System.currentTimeMillis() - Main.getStartTime()) / 1000d)) +
             " segundos");
-            System.exit(0);
+            // Trava a execucao no ultimo elemento
+            if(i++ == matches.length - 1){
+                System.exit(0);
+            }
         }
 	}
 	@Override
